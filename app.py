f36517aa0e739b008859d13fd065409346714f43
Wender1903 <wenderman74@gmail.com>
2025-08-20 17:47:40 -0300

    :sparkles: feat: Adicionando rota Post

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ def listar_usuario_por_id(id):
             return jsonify(usuario), 200
     return jsonify({'mensagem': 'Usuario nao encontrado'}), 404
 
+@app.route('/users', methods=['POST'])
+def criar_usuario():
+    novo_usuario = request.json
+    id = max(usuario['id'] for usuario in usuarios) + 1
+    novo_usuario['id'] = id
+    usuarios.append(novo_usuario)
+    return jsonify({'mensagem': 'Usuario cadastrado com sucesso'}), 201
 
 if __name__ == '__main__':
     app.run(host='localhost', port= 5000, debug= True)
