9e2d4ef39b8cd8d8a1a1d30470bd1f798a547a30
Wender1903 <wenderman74@gmail.com>
2025-08-20 17:40:28 -0300

    :sparkles: feat: Adicionando rota get por id

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/users', methods=['GET'])
 def listar_usuarios():
-    return jsonify(usuarios)
+    return jsonify(usuarios), 200
+
+@app.route('/users/<int:id>', methods=['GET'])
+def listar_usuario_por_id(id):
+    for usuario in usuarios:
+        if usuario['id'] == id:
+            return jsonify(usuario), 200
+    return jsonify({'mensagem': 'Usuario nao encontrado'}), 404
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port= 5000, debug= True)
